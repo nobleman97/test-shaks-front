2267c457fc57786d547e8cf89a5b5eab837c64ff
Desmondezo1 <desezo@gmail.com>
2024-03-24 16:24:34 +0000

    updates:
    replaced mailgun with resend.com
    created email templates

--- app/api/users/create/route.ts
+++ app/api/users/create/route.ts
@@ -1,14 +1,29 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+import SignupThankYouEmail from "@/app/components/email-templates/signup-success";
 
 export async function POST( req: NextRequest) {
     try {
 
         const body = await req.json();
+        const resend = new Resend(process.env.RESEND_API_KEY);
         
         const url = `${process.env.BACKEND_URL}/users`
         const user: any = await axios.post(url, body);
         const data = user.data
+
+        if(data){
+          //send email
+          const emailSent = await resend.emails.send({
+            from: 'Shakazu <onboarding@resend.dev>',
+            to: [body.email],
+            subject: 'Thank You for Contacting Us',
+            react: SignupThankYouEmail({   
+              name: body.firstname,  
+            }),
+          });
+        }
 
 
         return NextResponse.json({ message: "User created successfully", data }, {status: 200});
